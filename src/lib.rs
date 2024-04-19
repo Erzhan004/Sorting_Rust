d1af274132b245da8823c16dd72383d45d2980bd
Erzhan004 <124499458+Erzhan004@users.noreply.github.com>
2024-04-19 23:33:13 +0500

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod quick_sort;
+mod merge_sort;
+mod selection_sort;
+mod insertion_sort;
+
+pub use quick_sort::quick_sort;
+pub use merge_sort::merge_sort;
+pub use selection_sort::selection_sort;
+pub use insertion_sort::insertion_sort;
